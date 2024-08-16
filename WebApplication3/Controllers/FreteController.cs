78c47f826cdb41114d3c8c9bf02b14f55bf6c709
anton <anton@DESKTOP-O40QG55>
2024-08-16 17:40:33 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication3/Controllers/FreteController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+
+[ApiController]
+[Route("api/frete")]
+public class FreteController : ControllerBase
+{
+    private const float TaxaPorCm3 = 0.01f;
+
+    private static readonly Dictionary<string, float> TarifaPorEstado = new Dictionary<string, float>
+    {
+        { "SP", 50.0f },
+        { "RJ", 60.0f },
+        { "MG", 55.0f },
+        { "OUTROS", 70.0f }
+    };
+
+    [HttpPost]
+    public IActionResult CalcularFrete([FromBody] classfrete classfrete)
+    {
+        if (classfrete == null)
+        {
+            return BadRequest("Dados inválidos.");
+        }
+
+        float volume = classfrete.Altura * classfrete.Largura * classfrete.Comprimento;
+        float tarifaEstado = TarifaPorEstado.ContainsKey(classfrete.UF.ToUpper()) ?
+                              TarifaPorEstado[classfrete.UF.ToUpper()] :
+                              TarifaPorEstado["OUTROS"];
+
+        float valorFrete = (volume * TaxaPorCm3) + tarifaEstado;
+
+        return Ok(new
+        {
+            Classfrete = classfrete.Nome,
+            Volume = volume,
+            ValorFrete = valorFrete
+        });
+    }
+}
---
+++ WebApplication3/Models/classfrete.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication3.Models
+{
+    public class classfrete
+    {
+        public string Nome { get; set; } 
+        public float Peso { get; set; }
+        public float Altura { get; set; }
+        public float Largura { get; set; }
+        public float Comprimento { get; set; }
+        public string UF { get; set; }
+
+    }
+}
